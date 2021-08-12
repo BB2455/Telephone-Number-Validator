7ad3786e8d93e1f5fa83af063e43ef60f6de3945
Blair Burke <BlairBurke@Outlook.com>
2021-08-12 20:08:44 -0230

    first commit

---
+++ telephone-validator.js
@@ -0,0 +1,9 @@
+function telephoneCheck(str) {
+  let regex = /^1?[ -]?(\d{3}|\(\d{3}\))[ -]?\d{3}[ -]?\d{4}$/;
+  return regex.test(str);
+}
+
+console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("1 (555) 555-5555"));
+console.log(telephoneCheck("5555555555"));
+console.log(telephoneCheck("555-5555"));
